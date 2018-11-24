f929facc1455ca70fc96652bae616c457fc2223b
Diego Gama <ti.diegogama@gmail.com>
2018-11-23 23:11:05 -0200

    shift eureka server and client

---
+++ msconfigserver/src/main/java/com/fiap/shift/msconfigserver/MsconfigserverApplication.java
@@ -0,0 +1,12 @@
+package com.fiap.shift.msconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsconfigserverApplication.class, args);
+	}
+}
